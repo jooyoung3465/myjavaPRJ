5ac6d70a34b3460710c32d15777ec240bf2ba697
jungjuyoung <jooyoung3465@gmail.com>
2025-03-14 14:40:48 +0900

    변경

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!! My First Coding");
+        System.out.println("Hello!! My First Coding");
+        System.out.println("Hello!! My First Coding");
+        System.out.println("hi!! My First Coding");
+        System.out.println("Hello!! My First Coding");
+        System.out.println("Hello!! My First Coding");
+        System.out.println("Hello!! My First Coding");
+        System.out.println("Hello!! My First Coding");
     }
 }
